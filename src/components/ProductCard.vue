89d75a49819d39292ca757c8392c7ae4e3d7ae29
tommoko3ds <kenyaescamill@gmail.com>
2025-03-23 10:44:04 -0500

    card arreglada

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,21 +1,26 @@
 <template>
-      <div class="bg-[#C4D8BA] w-full h-full rounded-xl ">
-      <div class=" w-full h-[80%]  items-center justify-center p-2 flex flex-col">
-          <div class="w-full h-full bg-white rounded-xl relative"> <!--Restauracion de la linea de diseño, porque?. Demaciado grande la card.  22393139--> 
-          <img src="https://i5.walmartimages.com/seo/Michael-Kors-Voyager-Travel-Shoulder-Tote-Moss-Green-Small-Crossgrain-Leather_e25dc1b9-ccc6-48d0-a502-d56433c077c2.68903ed96f32417bada42b83a47fb364.jpeg" class="object-cover w-[20rem] h-auto rounded-t-md"/>
-        </div>
-        <div class="w-full h-[8rem]  px-2.5  flex flex-col gap-3">
+  <div class="bg-[#C4D8BA] w-full  h-[55vh] rounded-xl">
+    <div class="w-full pt-4 h-[70%] flex items-center justify-center">
+      <div class="w-[18rem] h-auto bg-white rounded-xl flex items-center justify-center">
+        <img
+          src="https://i5.walmartimages.com/seo/Michael-Kors-Voyager-Travel-Shoulder-Tote-Moss-Green-Small-Crossgrain-Leather_e25dc1b9-ccc6-48d0-a502-d56433c077c2.68903ed96f32417bada42b83a47fb364.jpeg"
+          class="object-cover w-[18rem] h-auto rounded-xl" />
 
-<p class="text-[#5B735D]">Titulo del objeto</p>
-<p class="text-[#749176] text-sm line-clamp-2">Aqui ira una pequeña descripcion del objeto a intercambiar, sera escrita por el usuario.</p>
-<div class="">
-  <button class="text-sm font-semibold py-1 px-2 bg-[#2f302f] text-white rounded-xl w-[6vw] text-center ">Etiqueta</button>
-</div>
-
-</div>
+        <!-- <button class="text-sm font-semibold py-1 px-2 bg-[#d63a85] text-white rounded-lg w-[8vw] text-center absolute left-2 bottom-2">Cancelar</button>
+      <button class="text-sm font-semibold py-1 px-2 bg-[#5d7e4e] text-white rounded-lg w-[8vw] text-center absolute right-2 bottom-2">Finalizar</button> -->
       </div>
-
     </div>
+    <div class="w-full h-[12.5%] px-[2rem] pt-1">
+      <p class="text-[#5B735D] font-bold">Titulo del objeto</p>
+      <p class="text-[#749176] text-sm line-clamp-2">Aqui ira una pequeña descripcion del objeto a intercambiar, sera
+        escrita por el usuario.</p>
+    </div>
+    <div class="w-full h-[12.5%] flex flex-wrap px-[2rem] py-2 gap-1">
+      <button class="text-sm font-semibold py-1 px-2 bg-black text-white rounded-xl w-fit h-fit text-center">Etiqueta</button>
+      <button class="text-sm font-semibold py-1 px-2 bg-black text-white rounded-xl w-fit h-fit text-center">Etiqueta</button>
+    </div>
+
+  </div>
 </template>
 
 <script lang="ts" setup>
