6c1d1e82fb64ff5ff152a0cab5774479bbb1dc43
Fernando <gomeztoledofernando14@gmail.com>
2025-03-24 20:46:33 -0500

    Se agrego el modal de registrar producto mejorado. 22393139 Fernando Gomez Toledo

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -1,7 +1,17 @@
 import { genericRequest } from '../utils/genericRequest'
+import type { IProduct } from '@/interfaces/IProduct';
 
 const base_url = 'https://localhost:7140/api' // --- Aqui estuvo el skill issue
 
 export const getProductsService = async () => {
   return await genericRequest(base_url + '/producto', 'GET')
-}
+};
+
+
+export const postProductService = async (data: any) => {
+  return await genericRequest(base_url + '/producto', 'POST', data);
+};
+
+export const getProductByIdService = async (id: number) => {
+  return await genericRequest(`${base_url}/producto/detail/${id}`, 'GET');
+};
