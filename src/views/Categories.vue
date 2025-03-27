b6be25e2b2c92f4c5119aed3989cafd9e2830cda
tommoko3ds <kenyaescamill@gmail.com>
2025-03-27 12:35:51 -0500

    front de categorias mejorado

--- src/views/Categories.vue
+++ src/views/Categories.vue
@@ -1,27 +1,37 @@
 <template>
-  <div class="w-full min-h-screen bg-[#FAF7EC] flex flex-col items-center p-8">
+  <div class="w-full min-h-screen bg-[#FAF7EC] flex flex-col items-center p-8 ">
+    <div class="  w-[100rem] h-[30rem] pb-12  items-center justify-center flex pt-80 mb-15">
+
+<img src="@/assets/Images/categorias.png" alt="Logo" class="w-[100rem] h-[70rem] pb-60 " />
+      </div>
+
+<!--
     <h1 class="text-5xl font-bold text-[#2C3E50] mb-8">Categorías</h1>
 <h2 class="text-2xl font-semibold text-[#50683E] text-center mb-8">
   Explora nuestras categorías y encuentra el trueque perfecto para ti.
-</h2>
-    <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-20 w-full max-w-7xl">
-  <RouterLink
-    v-for="(category, index) in categories"
-    :key="index"
-    :to="`/categories/${category.nombre}`"
-    class="bg-white shadow-lg rounded-xl w-72 h-72 flex flex-col items-center justify-center cursor-pointer transition-transform transform hover:scale-110 overflow-hidden relative"
+</h2> -->
+    <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-12 w-full max-w-[95rem] ">
+      <RouterLink
+  v-for="(category, index) in categories"
+  :key="index"
+  :to="`/categories/${category.nombre}`"
+  class="bg-white shadow-lg rounded-xl w-82 h-126 flex flex-col items-center justify-center cursor-pointer transition-transform transform hover:scale-110 overflow-hidden relative"
+>
+<img
+  :src="category.imagenCategoria"
+  :alt="category.nombre"
+  class="w-full h-full object-cover filter blur-[2.2px] transition-all duration-300 hover:blur-none"
+/>
+
+
+  <div
+    class="absolute w-[14rem] h-[18rem] bg-[#94b17e] text-[#07330799] text-center flex justify-center items-center text-lg font-bold rounded-2xl hover:bg-[#d6f3bb] hover:opacity-100 "
   >
-    <img
-      :src="category.imagenCategoria"
-      :alt="category.nombre"
-      class="w-full h-full object-cover"
-    />
-    <div
-      class="absolute bottom-0 w-full bg-[#50683E] bg-opacity-70 text-white text-center py-3 text-lg font-semibold"
-    >
-      {{ category.nombre }}
-    </div>
-  </RouterLink>
+    {{ category.nombre }}
+  </div>
+</RouterLink>
+
+
 </div>
 
   </div>
