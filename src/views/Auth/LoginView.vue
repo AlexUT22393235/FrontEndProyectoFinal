d26ba0380baf219737579e951682660cd1a5ce87
AlexUT22393235 <22393235@utcancun.edu.mx>
2025-03-30 18:37:09 -0500

    Implementación de responsive para login y register. Alex

--- src/views/Auth/LoginView.vue
+++ src/views/Auth/LoginView.vue
@@ -44,41 +44,42 @@ const handleLogin = async () => {
 </script>
 
 <template>
-  <div class="bg-[#4b6934] w-full h-screen flex flex-row justify-center items-center">
-    <div class="flex justify-center items-center w-1/2 h-full bg-[#264026]">
+  <div class="bg-[#4b6934] w-full h-screen flex flex-col lg:flex-row justify-center items-center">
+    <div class="hidden lg:flex justify-center items-center lg:w-1/2 h-full bg-[#264026]">
       <div class="w-[40rem] h-[40rem]">
         <img src="@/assets/Images/noTrash.jpeg" alt="Logo" class="w-[40rem] h-[40rem]" />
       </div>
     </div>
-    <div class="main-container flex flex-col justify-center items-center w-1/2 h-[50rem] bg-[#50683E]">
+    
+    <div class="main-container w-full lg:w-1/2 h-full lg:h-[50rem] flex flex-col justify-center items-center bg-[#50683E]">
       <div class="m-4 p-4">
         <h1 class="text-3xl font-bold text-[#FAF7EC] text-center">Login Page</h1>
       </div>
 
-      <div class="bg-[#FAF7EC] w-[30rem] h-[30rem] flex flex-col justify-center items-center rounded-lg">
+      <div class="bg-[#FAF7EC] w-[90%] sm:w-[80%] lg:w-[30rem] h-auto lg:h-[30rem] flex flex-col justify-center items-center rounded-lg p-6 lg:p-0">
         <input
           type="text"
-          class="w-[20rem] h-[2rem] border-2 border-[#50683E] rounded-md mb-2"
+          class="w-full lg:w-[20rem] h-[2.5rem] lg:h-[2rem] border-2 border-[#50683E] rounded-md mb-4 lg:mb-2 p-2"
           placeholder="Email"
           v-model="email"
           required
         >
         <input
           type="password"
-          class="w-[20rem] h-[2rem] border-2 border-[#50683E] rounded-md mb-2"
+          class="w-full lg:w-[20rem] h-[2.5rem] lg:h-[2rem] border-2 border-[#50683E] rounded-md mb-6 lg:mb-2 p-2"
           placeholder="Password"
           v-model="password"
           required
         >
         <button
-          class="w-[20rem] h-[2rem] bg-[#1a493d] text-[#FAF7EC] rounded-md"
+          class="w-full lg:w-[20rem] h-[2.5rem] lg:h-[2rem] bg-[#1a493d] text-[#FAF7EC] rounded-md"
           @click="handleLogin"
         >
           Login
         </button>
-        <div class="flex flex-row gap-2 mt-2">
-          <p>Aún no tienes una cuenta?</p>
-          <router-link to="/register" class="text-[#3d491a]">¡ÚNETE!</router-link>
+        <div class="flex flex-row gap-2 mt-6 lg:mt-2">
+          <p class="text-sm lg:text-base">Aún no tienes una cuenta?</p>
+          <router-link to="/register" class="text-[#3d491a] text-sm lg:text-base">¡ÚNETE!</router-link>
         </div>
       </div>
     </div>
@@ -90,11 +91,22 @@ const handleLogin = async () => {
   background: url('@/assets/Images/nuevo.png') center center;
   background-repeat: no-repeat;
   background-size: cover;
-  height: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   opacity: 0.9;
+}
+
+@media (max-width: 1023px) {
+  .main-container {
+    min-height: 100vh;
+    padding: 1rem;
+  }
+  
+  div.bg-\[\#FAF7EC\] {
+    margin: 1rem 0;
+    padding: 2rem 1rem;
+  }
 }
 </style>
