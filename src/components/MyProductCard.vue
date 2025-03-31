39bd9b3bf7640ccb4be4bb4127e6334e86a54aea
tommoko3ds <kenyaescamill@gmail.com>
2025-03-30 19:10:22 -0500

    front de mis productos

--- src/components/MyProductCard.vue
+++ src/components/MyProductCard.vue
@@ -1,13 +1,7 @@
 <template>
     <div class="bg-[#C4D8BA] w-full h-[55vh] rounded-xl transition-all duration-300 hover:scale-105">
-      <RouterLink :to="`/products/${props.id}`">
-        ícono de ojo aquí
-      </RouterLink>
-      <div>
-        <button @click="emit('delete-request', props.id)" class="bg-red-500 text-white px-4 py-2 rounded-md">
-          x
-        </button>
-      </div>
+
+
       <div class="w-full pt-4 h-[70%] flex items-center justify-center">
         <div class="w-[18rem] h-[18rem] bg-white rounded-xl flex items-center justify-center shadow-lg">
           <img :src="props.imgSrc" class="object-cover w-full h-full rounded-xl" />
@@ -21,23 +15,45 @@
           <slot name="description"></slot>
         </p>
       </div>
+      <div class="flex flex-row gap-2 px-60 ">
+        <RouterLink :to="`/products/${props.id}`">
+        <div class="bg-[#5d7e4e] text-white px-4 py-2 rounded-md w-14">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+  <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+</svg>
+
+        </div>
+
+
+      </RouterLink>
+      <div >
+        <button @click="emit('delete-request', props.id)" class="bg-red-500 text-white px-4 py-2 rounded-md">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+</svg>
+
+        </button>
+      </div>
+      </div>
       <div class="w-full h-[12.5%] flex flex-wrap px-[2rem] py-2 gap-1">
         <button v-for="(item, index) in props.categories.slice(0, 4)" :key="index" class="text-sm font-semibold py-1 px-2 bg-black text-white rounded-xl w-fit h-fit text-center">
           {{ item.nombre }}
         </button>
       </div>
+
     </div>
   </template>
-  
-  <script lang="ts" setup>
+
+  <script setup lang="ts" >
   import type { ICategory } from '@/interfaces/ICategory';
   import { RouterLink } from 'vue-router';
-  
+
   const props = defineProps<{
     imgSrc: string;
     id: number;
     categories: ICategory[];
   }>();
-  
+
   const emit = defineEmits(['delete-request']);
   </script>
--- src/views/MyProductsView.vue
+++ src/views/MyProductsView.vue
@@ -1,7 +1,11 @@
 <template>
-    <div class="w-full min-h-screen bg-[#FAF7EC] relative">
+    <div class="w-full min-h-screen bg-[#FAF7EC] relative px-10 pt-4 ">
       <h3 class="text-bold text-6xl pb-4">Mis Productos</h3>
-      <button @click="isModalOpen = true">Agregar Producto</button>
+
+      <div class="bg-[#5d7e4e] w-50 text-center px-2 p-4 rounded-md text-white">
+        <button @click="isModalOpen = true">Agregar Producto</button>
+      </div>
+
       <div class="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-4 px-4 py-6">
         <MyProductCard
           v-for="(item, index) in productsStore.productsPerUser"
@@ -23,7 +27,7 @@
       <DeleteProductModal v-if="isDeleteModalOpen" :product-id="productIdToDelete" @close="isDeleteModalOpen = false" @product-deleted="handleProductDeleted" />
     </div>
   </template>
-  
+
   <script setup lang="ts">
   import MyProductCard from '@/components/MyProductCard.vue';
   import { useProductStore } from '@/stores/productStore';
@@ -32,23 +36,23 @@
   import { storeToRefs } from 'pinia';
   import AddProductModal from '@/components/Modals/AddProductModal.vue';
   import DeleteProductModal from '@/components/Modals/DeleteProductModal.vue';
-  
+
   const productsStore = useProductStore();
   const authStore = useAuthStore();
   const { user } = storeToRefs(authStore);
-  
+
   const isModalOpen = ref(false);
   const isDeleteModalOpen = ref(false);
   const productIdToDelete = ref(0);
-  
+
   const handleDeleteRequest = (productId: number) => {
     productIdToDelete.value = productId;
     isDeleteModalOpen.value = true;
   };
-  
+
   const handleProductDeleted = () => {
   };
-  
+
   onMounted(async () => {
     if (user.value && user.value.id) {
       await productsStore.fetchProductsByUser(user.value.id);
