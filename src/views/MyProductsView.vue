b88baf285b1506a7d606089141bfd08c30d1e5b6
tommoko3ds <kenyaescamill@gmail.com>
2025-03-30 20:36:52 -0500

    front de mis productos

--- src/views/MyProductsView.vue
+++ src/views/MyProductsView.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-full min-h-screen bg-[#FAF7EC] relative px-10 pt-4 ">
+    <div class="items-center justify-center align-middle w-full min-h-screen bg-[#FAF7EC] relative px-10 pt-4 ">
       <div class="bg-amber-500 w-full h-50rem flex items-center justify-center text-center py-20 m-4">
 
         <h3 class="text-bold text-6xl pb-4n text-[#FAF7EC]">Mis Productos</h3>
@@ -10,9 +10,9 @@
       <div class="bg-[#5d7e4e] w-50 text-center px-2 p-4 rounded-md text-white">
         <button @click="isModalOpen = true">Agregar Producto</button>
       </div>
+      <div v-if="productsStore.productsPerUser.length > 0" class="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-4 px-4 py-6 bg-amber-950">
+      <div class="justify-center items-center align-middle w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-4 px-4 py-6">
 
-      <div class="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-4 px-4 py-6">
-        <div v-if="productsStore.productsPerUser.length > 0" class="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-4 px-4 py-6">
           <MyProductCard
           v-for="(item, index) in productsStore.productsPerUser"
           :key="index"
@@ -31,11 +31,11 @@
 
 
         </div>
-        <div v-else class="w-full text-center my-4 text-gray-600">
+
+      </div>
+      <div v-else class="w-full text-center my-4 text-gray-600  text-2xl " >
       No tienes productos disponibles. Haz click en agregar producto para comenzar !
     </div>
-      </div>
-
       <AddProductModal v-if="isModalOpen" :is-open="isModalOpen" @close="isModalOpen = false" />
       <DeleteProductModal v-if="isDeleteModalOpen" :product-id="productIdToDelete" @close="isDeleteModalOpen = false" @product-deleted="handleProductDeleted" />
     </div>
